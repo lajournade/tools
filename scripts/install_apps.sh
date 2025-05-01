928198e68f22980b769db955845e4b94ccdd0e17
lajournade <lajournade@proton.me>
2025-05-01 11:34:28 +0200

    fixup! Add script to install apps

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -3,10 +3,11 @@
 echo "✅ Mise à jour des dépôts..."
 sudo apt update
 
+# --- apt packages ---
 echo "📦 Installation des paquets APT..."
 xargs -a apt_apps.txt sudo apt install -y
 
-# --- DISCORD --
+# --- Discord ---
 if ! command -v discord &> /dev/null; then
 	echo "🎮 Installation de Discord (paquet .deb)..."
 	wget -O discord.deb "https://discord.com/api/download?platform=linux&format=deb"
@@ -14,12 +15,37 @@ if ! command -v discord &> /dev/null; then
 	rm discord.deb
 fi
 
-# --- BITWARDEN --
+# --- Bitwarden ---
 if ! command -v bitwarden &> /dev/null; then
 	echo "🔐 Installation de Bitwarden (paquet .deb)..."
 	wget -O bitwarden.deb "https://bitwarden.com/download/?app=desktop&platform=linux&variant=deb"
 	sudo apt install -y ./bitwarden.deb
 	rm bitwarden.deb
+fi
+
+# --- Fastfetch ---
+if ! command -v fastfetch &> /dev/null; then
+	echo "📦 Installation de Fastfetch..."
+	add-apt-repository ppa:zhangsongcui3371/fastfetch
+	sudo apt update
+	sudo apt install fastfetch
+fi
+
+# --- oh my posh ---
+if ! command -v oh-my-posh &> /dev/null; then
+	echo "📦 Installation de oh-my-posh..."
+	mkdir -p ~/.local/bin
+	curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+fi
+
+# --- Nerdy font ---
+if [[ ! -f .local/share/fonts/SymbolsNerdFont-Regular.ttf ]]; then
+	echo "📝 Installation de Nerdy font..."
+	mkdir -p ~/.local/share/fonts
+	cd ~/.local/share/fonts
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/NerdFontsSymbolsOnly.zip
+	unzip NerdFontsSymbolsOnly.zip
+	rm NerdFontsSymbolsOnly.zip
 fi
 
 echo "✅ Installation terminée."
