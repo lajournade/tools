4806020baabe4c06e8fd565bb710cf069975dc7a
lajournade <lajournade@proton.me>
2025-05-01 10:44:26 +0200

    Add git configuration script

---
+++ Scripts/git_conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# [Global]
+git config --global core.editor "vim"
+
+# [Alias]
+git config --global alias.s "status"
+git config --global alias.a "add"
+git config --global alias.p "push"
+git config --global alias.pf "push --force"
