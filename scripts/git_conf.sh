f4f66a1461b9194884dc78362ffaa91af66fd61b
lajournade <lajournade@proton.me>
2025-05-01 10:47:19 +0200

    fixup! Add git configuration script

--- scripts/git_conf.sh
+++ scripts/git_conf.sh
@@ -6,5 +6,6 @@ git config --global core.editor "vim"
 # [Alias]
 git config --global alias.s "status"
 git config --global alias.a "add"
+git config --global alias.c "commit -m"
 git config --global alias.p "push"
 git config --global alias.pf "push --force"
